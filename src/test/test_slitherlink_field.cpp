bb24e7bc45886e4f80f8618f3ec55373d59bc1a1
semiexp <linthya.v@gmail.com>
2016-01-27 10:36:47 +0900

    Slitherlink: add tests

--- src/test/test_slitherlink_field.cpp
+++ src/test/test_slitherlink_field.cpp
@@ -21,11 +21,43 @@ void SlitherlinkFieldAddClue()
 	Database db;
 	db.CreateDefault();
 
-	Field field(3, 3);
-	field.SetDatabase(&db);
-	field.AddClue(Position(1, 1), 0);
+	{
+		Field field(3, 3);
+		field.SetDatabase(&db);
+		field.AddClue(Position(1, 1), 0);
 
-	assert(field.GetEdge(Position(2, 3)) == Field::EDGE_BLANK);
+		assert(field.GetEdge(Position(2, 3)) == Field::EDGE_BLANK);
+		assert(field.GetEdge(Position(3, 2)) == Field::EDGE_BLANK);
+		assert(field.GetEdge(Position(4, 3)) == Field::EDGE_BLANK);
+		assert(field.GetEdge(Position(3, 4)) == Field::EDGE_BLANK);
+	}
+
+	{
+		Field field(3, 3);
+		field.SetDatabase(&db);
+		field.AddClue(Position(0, 0), 1);
+
+		assert(field.GetEdge(Position(0, 1)) == Field::EDGE_BLANK);
+		assert(field.GetEdge(Position(1, 0)) == Field::EDGE_BLANK);
+	}
+
+	{
+		Field field(3, 3);
+		field.SetDatabase(&db);
+		field.AddClue(Position(0, 0), 2);
+
+		assert(field.GetEdge(Position(0, 3)) == Field::EDGE_LINE);
+		assert(field.GetEdge(Position(3, 0)) == Field::EDGE_LINE);
+	}
+
+	{
+		Field field(3, 3);
+		field.SetDatabase(&db);
+		field.AddClue(Position(0, 0), 3);
+
+		assert(field.GetEdge(Position(0, 1)) == Field::EDGE_LINE);
+		assert(field.GetEdge(Position(1, 0)) == Field::EDGE_LINE);
+	}
 }
 }
 }
