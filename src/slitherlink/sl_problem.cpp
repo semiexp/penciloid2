8c9841e85584e94c2501e6d9b500ebe3bd399d14
semiexp <linthya.v@gmail.com>
2016-02-02 09:48:56 +0900

    Slitherlink: bug fix in Problem

--- src/slitherlink/sl_problem.cpp
+++ src/slitherlink/sl_problem.cpp
@@ -21,14 +21,17 @@ Problem::Problem(Y height, X width, const char* clues[]) : field_clue_(nullptr),
 		for (X x(0); x < width; ++x) {
 			if ('0' <= clues[y][x] && clues[y][x] <= '3') {
 				SetClue(Position(y, x), clues[y][x] - '0');
+			} else {
+				SetClue(Position(y, x), kNoClue);
 			}
 		}
 	}
 }
 Problem::Problem(const Problem &other) : field_clue_(nullptr), height_(other.height_), width_(other.width_)
 {
-	field_clue_ = new Clue[static_cast<int>(height()) * static_cast<int>(width())];
-	std::copy(other.field_clue_, other.field_clue_ + static_cast<int>(height()) * static_cast<int>(width()), field_clue_);
+	int cell_count = static_cast<int>(height()) * static_cast<int>(width());
+	field_clue_ = new Clue[cell_count];
+	for (int i = 0; i < cell_count; ++i) field_clue_[i] = other.field_clue_[i];
 }
 Problem::Problem(Problem &&other) : field_clue_(other.field_clue_), height_(other.height_), width_(other.width_)
 {
