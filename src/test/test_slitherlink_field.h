56c8b504339bd0b7424f7adecdf5847eb1b99e43
semiexp <linthya.v@gmail.com>
2016-02-17 15:42:52 +0900

    Slitherlink: don't create Database several times

--- src/test/test_slitherlink_field.h
+++ src/test/test_slitherlink_field.h
@@ -1,13 +1,15 @@
 #pragma once
 
+#include "../slitherlink/sl_database.h"
+
 namespace penciloid
 {
 namespace test
 {
-void SlitherlinkFieldAddClue();
-void SlitherlinkFieldTheorem();
-void SlitherlinkFieldFullySolvableProblem();
-void SlitherlinkFieldSolveProblem();
-void SlitherlinkFieldDiagonalChain();
+void SlitherlinkFieldAddClue(penciloid::slitherlink::Database &db);
+void SlitherlinkFieldTheorem(penciloid::slitherlink::Database &db);
+void SlitherlinkFieldFullySolvableProblem(penciloid::slitherlink::Database &db);
+void SlitherlinkFieldSolveProblem(penciloid::slitherlink::Database &db);
+void SlitherlinkFieldDiagonalChain(penciloid::slitherlink::Database &db);
 }
 }
