967d58ffe7fb174e160c7ccd42e4dcdd15886f27
semiexp <linthya.v@gmail.com>
2016-03-13 21:58:02 +0900

    Slitherlink: make the usage of methods configurable

--- src/slitherlink/sl_generator_option.h
+++ src/slitherlink/sl_generator_option.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "sl_database.h"
+#include "sl_method.h"
 
 namespace penciloid
 {
@@ -8,9 +9,10 @@ namespace slitherlink
 {
 struct GeneratorOption
 {
-	GeneratorOption() : use_assumption(false), field_database(nullptr) {}
+	GeneratorOption() : use_assumption(false), method(), field_database(nullptr) {}
 
 	bool use_assumption;
+	Method method;
 	Database *field_database;
 };
 }
