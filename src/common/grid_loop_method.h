2b1073c1e27a7397345370e9552f3d3270fa87f0
semiexp <linthya.v@gmail.com>
2016-03-17 00:42:34 +0900

    Add DisableAll to GridLoopMethod, slitherlink::Method

--- src/common/grid_loop_method.h
+++ src/common/grid_loop_method.h
@@ -8,6 +8,12 @@ struct GridLoopMethod
 		avoid_three_lines(true), avoid_cycle(true)
 	{}
 
+	void DisableAll()
+	{
+		avoid_three_lines = false;
+		avoid_cycle = false;
+	}
+
 	bool avoid_three_lines, avoid_cycle;
 };
 }
--- src/slitherlink/sl_method.h
+++ src/slitherlink/sl_method.h
@@ -12,6 +12,15 @@ struct Method
 		around_cell(true), adjacent_3(true), diagonal_3(true), diagonal_chain(true)
 	{}
 
+	void DisableAll()
+	{
+		grid_loop_method.DisableAll();
+		around_cell = false;
+		adjacent_3 = false;
+		diagonal_3 = false;
+		diagonal_chain = false;
+	}
+
 	GridLoopMethod grid_loop_method;
 	bool around_cell, adjacent_3, diagonal_3, diagonal_chain;
 };
