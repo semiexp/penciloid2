4f653c4c846f577c85b73047a81f4201be9f31f4
semiexp <linthya.v@gmail.com>
2016-06-07 12:39:04 +0900

    Masyu: add a test

--- src/test/test_masyu_field.cpp
+++ src/test/test_masyu_field.cpp
@@ -12,6 +12,7 @@ namespace test
 void RunAllMasyuFieldTest()
 {
 	MasyuFieldAddClueTest();
+	MasyuFieldTheoremTest();
 }
 void MasyuFieldAddClueTest()
 {
@@ -34,6 +35,34 @@ void MasyuFieldAddClueTest()
 	field.AddClue(CellPosition(Y(4), X(2)), kClueWhite);
 	assert(field.GetEdge(LoopPosition(Y(8), X(3))) == Field::kEdgeLine);
 	assert(field.GetEdge(LoopPosition(Y(8), X(5))) == Field::kEdgeLine);
+}
+void MasyuFieldTheoremTest()
+{
+	using namespace masyu;
+
+	{
+		Field field(Y(7), X(7));
+		field.AddClue(CellPosition(Y(2), X(2)), kClueBlack);
+		field.AddClue(CellPosition(Y(2), X(3)), kClueBlack);
+
+		assert(field.GetEdge(LoopPosition(Y(4), X(3))) == Field::kEdgeLine);
+		assert(field.GetEdge(LoopPosition(Y(4), X(1))) == Field::kEdgeLine);
+		assert(field.GetEdge(LoopPosition(Y(4), X(7))) == Field::kEdgeLine);
+		assert(field.GetEdge(LoopPosition(Y(4), X(9))) == Field::kEdgeLine);
+	}
+	for (int i = 0; i < 3; ++i) {
+		Field field(Y(7), X(7));
+		field.AddClue(CellPosition(Y(2), X(2 + i % 3)), kClueWhite);
+		field.AddClue(CellPosition(Y(2), X(2 + (i + 1) % 3)), kClueWhite);
+		field.AddClue(CellPosition(Y(2), X(2 + (i + 2) % 3)), kClueWhite);
+
+		assert(field.GetEdge(LoopPosition(Y(3), X(4))) == Field::kEdgeLine);
+		assert(field.GetEdge(LoopPosition(Y(5), X(4))) == Field::kEdgeLine);
+		assert(field.GetEdge(LoopPosition(Y(3), X(6))) == Field::kEdgeLine);
+		assert(field.GetEdge(LoopPosition(Y(5), X(6))) == Field::kEdgeLine);
+		assert(field.GetEdge(LoopPosition(Y(3), X(8))) == Field::kEdgeLine);
+		assert(field.GetEdge(LoopPosition(Y(5), X(8))) == Field::kEdgeLine);
+	}
 }
 }
 }
--- src/test/test_masyu_field.h
+++ src/test/test_masyu_field.h
@@ -5,5 +5,6 @@ namespace penciloid
 namespace test
 {
 void MasyuFieldAddClueTest();
+void MasyuFieldTheoremTest();
 }
 }
