3548468bd24fc225aafb96ff03eb2a5641a96c08
semiexp <linthya.v@gmail.com>
2016-06-16 00:09:06 +0900

    Nurikabe: add Problem

---
+++ src/nurikabe/nk_problem.cpp
@@ -0,0 +1,32 @@
+#include "nk_problem.h"
+
+#include <xutility>
+
+namespace penciloid
+{
+namespace nurikabe
+{
+Problem::Problem() : grid_()
+{
+}
+Problem::Problem(Y height, X width) : grid_(height, width, kNoClue)
+{
+}
+Problem::Problem(const Problem &other) : grid_(other.grid_)
+{
+}
+Problem::Problem(Problem &&other) : grid_(std::move(other.grid_))
+{
+}
+Problem &Problem::operator=(const Problem &other)
+{
+	grid_ = other.grid_;
+	return *this;
+}
+Problem &Problem::operator=(Problem &&other)
+{
+	grid_ = std::move(other.grid_);
+	return *this;
+}
+}
+}
---
+++ src/nurikabe/nk_problem.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "nk_type.h"
+#include "../common/grid.h"
+
+namespace penciloid
+{
+namespace nurikabe
+{
+class Problem
+{
+public:
+	Problem();
+
+	// Initialize a problem with the empty board (each cell is initialized with kNoClue)
+	Problem(Y height, X width);
+	Problem(const Problem &other);
+	Problem(Problem &&other);
+	~Problem() {}
+
+	Problem &operator=(const Problem &other);
+	Problem &operator=(Problem &&other);
+
+	inline Y height() const { return grid_.height(); }
+	inline X width() const { return grid_.width(); }
+	inline void SetClue(CellPosition cell, Clue clue) { grid_.at(cell) = clue; }
+	inline Clue GetClue(CellPosition cell) const { return grid_.at(cell); }
+
+private:
+	Grid<Clue> grid_;
+};
+}
+}
