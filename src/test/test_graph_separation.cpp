d176c6fbababcb3c6c66bf4d4709510cc930c3f1
semiexp <linthya.v@gmail.com>
2016-06-16 20:26:25 +0900

    GraphSeperation: add tests

--- src/test/test_graph_separation.cpp
+++ src/test/test_graph_separation.cpp
@@ -2,9 +2,11 @@
 #include "test.h"
 
 #include <vector>
+#include <algorithm>
 #include <cassert>
 
 #include "../common/graph_separation.h"
+#include "../common/union_find.h"
 
 namespace penciloid
 {
@@ -12,36 +14,88 @@ namespace test
 {
 void RunAllGraphSeparationTest()
 {
-	GraphSeparationTest1();
+	GraphSeparationTest();
 }
-void GraphSeparationTest1()
+void GraphSeparationRunTestCase(int n_vertex, const std::vector<std::pair<int, int> > &edges)
 {
-	int v = 6, e = 6;
-	GraphSeparation<int> graph(v, e);
-	for (int i = 0; i < v; ++i) graph.SetValue(i, 1 << i);
-
-	graph.AddEdge(0, 1);
-	graph.AddEdge(0, 2);
-	graph.AddEdge(1, 2);
-	graph.AddEdge(1, 4);
-	graph.AddEdge(2, 3);
-	graph.AddEdge(2, 5);
-
+	GraphSeparation<int> graph(n_vertex, edges.size());
+	for (int i = 0; i < n_vertex; ++i) graph.SetValue(i, 1 << i);
+	for (auto &e : edges) graph.AddEdge(e.first, e.second);
 	graph.Construct();
+	
+	UnionFind uf_base(n_vertex);
+	for (auto &e : edges) uf_base.Join(e.first, e.second);
 
-	std::vector<int> expected[] = {
-		{0b111110},
-		{0b010000, 0b101101},
-		{0b001000, 0b010011, 0b100000},
-		{0b110111},
-		{0b101111},
-		{0b011111}
-	};
-	for (int i = 0; i < v; ++i) {
-		std::vector<int> ans = graph.Separate(i);
-		sort(ans.begin(), ans.end());
-		assert(ans == expected[i]);
+	for (int p = 0; p < n_vertex; ++p) {
+		UnionFind uf(n_vertex);
+		for (auto &e : edges) {
+			if (e.first != p && e.second != p) uf.Join(e.first, e.second);
+		}
+		std::vector<int> expected;
+		for (int i = 0; i < n_vertex; ++i) {
+			if (i != p && uf.Root(i) == i && uf_base.Root(i) == uf_base.Root(p)) {
+				int val = 0;
+				for (int j = 0; j < n_vertex; ++j) {
+					if (uf.Root(j) == i) val += 1 << j;
+				}
+				expected.push_back(val);
+			}
+		}
+		std::vector<int> result = graph.Separate(p);
+		sort(expected.begin(), expected.end());
+		sort(result.begin(), result.end());
+		assert(expected == result);
 	}
+}
+void GraphSeparationTest()
+{
+	std::vector<std::pair<int, int> > ordinary_graph = {
+		{ 0, 1 },
+		{ 0, 2 },
+		{ 1, 2 },
+		{ 1, 4 },
+		{ 2, 3 },
+		{ 2, 5 }
+	};
+	// Ordinary graph
+	GraphSeparationRunTestCase(6, ordinary_graph);
+	
+	// Multiple edges
+	for (int i = 0; i < ordinary_graph.size(); ++i) {
+		auto multiple_edges = ordinary_graph;
+		multiple_edges.push_back(ordinary_graph[i]);
+		GraphSeparationRunTestCase(6, multiple_edges);
+	}
+
+	// Self edges
+	for (int i = 0; i < 6; ++i) {
+		auto self_edges = ordinary_graph;
+		self_edges.push_back({ i, i });
+		GraphSeparationRunTestCase(6, self_edges);
+	}
+
+	// Tree
+	GraphSeparationRunTestCase(6, {
+		{ 0, 1 },
+		{ 1, 2 },
+		{ 0, 3 },
+		{ 3, 5 },
+		{ 5, 4 }
+	});
+
+	// Disconnected
+	GraphSeparationRunTestCase(8, {
+		{ 0, 1 },
+		{ 1, 2 },
+		{ 1, 3 },
+		{ 2, 3 },
+		{ 4, 5 },
+		{ 4, 7 }
+	});
+
+	// Empty graph
+	GraphSeparationRunTestCase(5, {
+	});
 }
 }
 }
--- src/test/test_graph_separation.h
+++ src/test/test_graph_separation.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <vector>
+#include <algorithm>
+
 namespace penciloid
 {
 namespace test
 {
-void GraphSeparationTest1();
+void GraphSeparationRunTestCase(int n_vertex, const std::vector<std::pair<int, int> > &edges);
+void GraphSeparationTest();
 }
 }
