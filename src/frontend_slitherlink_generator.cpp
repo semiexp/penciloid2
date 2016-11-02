116cbc29e547b4bc37c743d4e99f5fc0a232e3ed
semiexp <linthya.v@gmail.com>
2016-11-02 23:10:44 +0900

    frontend_slitherlink_generator: add error detection

--- src/frontend_slitherlink_generator.cpp
+++ src/frontend_slitherlink_generator.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <random>
+#include <sstream>
 
 #include "slitherlink/sl_dictionary.h"
 #include "slitherlink/sl_generator.h"
@@ -84,11 +85,23 @@ int main(int argc, char** argv)
 			// option ended
 			break;
 		}
+		bool is_next_int = false;
+		int val_next_int;
+		if (arg_idx + 1 < argc) {
+			std::istringstream iss(argv[arg_idx + 1]);
+			iss >> val_next_int;
+			if (!iss.fail()) is_next_int = true;
+		}
+
 		char opt = argv[arg_idx][1];
 		switch (opt)
 		{
 		case 'o':
 			// specify the output file
+			if (arg_idx + 1 >= argc) {
+				std::cerr << "error: missing value after -o" << std::endl;
+				return 0;
+			}
 			out_filename = argv[arg_idx + 1];
 			++arg_idx;
 			break;
@@ -98,22 +111,38 @@ int main(int argc, char** argv)
 			break;
 		case 'h':
 			// specify the height of the generated problem (enabled only if '-a' is specified)
-			height = atoi(argv[arg_idx + 1]);
+			if (!is_next_int) {
+				std::cerr << "error: missing integer value after -h" << std::endl;
+				return 0;
+			}
+			height = val_next_int;
 			++arg_idx;
 			break;
 		case 'w':
 			// specify the width of the generated problem (enabled only if '-a' is specified)
-			width = atoi(argv[arg_idx + 1]);
+			if (!is_next_int) {
+				std::cerr << "error: missing integer value after -w" << std::endl;
+				return 0;
+			}
+			width = val_next_int;
 			++arg_idx;
 			break;
 		case 'c':
 			// specify the minimum number of the clues
-			n_clue_lo = atoi(argv[arg_idx + 1]);
+			if (!is_next_int) {
+				std::cerr << "error: missing integer value after -c" << std::endl;
+				return 0;
+			}
+			n_clue_lo = val_next_int;
 			++arg_idx;
 			break;
 		case 'C':
 			// specify the maximum number of the clues
-			n_clue_hi = atoi(argv[arg_idx + 1]);
+			if (!is_next_int) {
+				std::cerr << "error: missing integer value after -C" << std::endl;
+				return 0;
+			}
+			n_clue_hi = val_next_int;
 			++arg_idx;
 			break;
 		default:
@@ -145,18 +174,32 @@ int main(int argc, char** argv)
 
 	if (!gen_clue_auto) {
 		std::ifstream ifs(in_filename);
+		if (ifs.fail()) {
+			std::cerr << "error: couldn't open file '" << in_filename << "'" << std::endl;
+			return 0;
+		}
 		ifs >> height >> width;
-
+		if (ifs.fail() || height <= 0 || width <= 0) {
+			std::cerr << "error: invalid input format" << std::endl;
+			return 0;
+		}
 		CluePlacement clue_placement{ Y(height), X(width) };
 		for (Y y(0); y < height; ++y) {
 			for (X x(0); x < width; ++x) {
 				std::string token;
 				ifs >> token;
+				if (ifs.fail()) {
+					std::cerr << "error: invalid input format" << std::endl;
+					return 0;
+				}
 
 				if (token[0] >= '0' && token[0] <= '3') {
 					clue_placement.SetClue(CellPosition(y, x), Clue(static_cast<int>(token[0] - '0')));
 				} else if (token[0] == '5') {
 					clue_placement.SetClue(CellPosition(y, x), kSomeClue);
+				} else {
+					std::cerr << "error: invalid input format" << std::endl;
+					return 0;
 				}
 			}
 		}
@@ -174,6 +217,10 @@ int main(int argc, char** argv)
 		out_filename = in_filename + ".generated.txt";
 	}
 	std::ofstream ofs(out_filename);
+	if (!ofs.good()) {
+		std::cerr << "error: couldn't open file '" << in_filename << "'" << std::endl;
+		return 0;
+	}
 	ofs << height << std::endl;
 	ofs << width << std::endl;
 	for (Y y(0); y < height; ++y) {
